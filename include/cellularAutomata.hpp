c8b1b94215c062e52bdf4849c7eaae8729fdfb19
winter <vortetty@gmail.com>
2022-02-10 09:50:10 -0800

    More automata work

--- include/cellularAutomata.hpp
+++ include/cellularAutomata.hpp
@@ -8,22 +8,43 @@
 #include "PractRand/RNGs/sfc64.h"
 #include "utils.hpp"
 
+struct automataGrid { 
+    std::deque<std::deque<uint8_t>> _grid;
+    vec2 _size = {10, 10};
+};
+
+struct automataRule { // Defaults to life
+    std::deque<uint8_t> _birth = {3};
+    std::deque<uint8_t> _survival = {2, 3};
+};
+
 class ruleAutomata {
 public:
     ruleAutomata(); // Initialize to B3/S23 or the normal life rules, 10x10 grid
     ruleAutomata(vec2 gridSize, std::deque<std::deque<uint8_t>> grid);
+    ruleAutomata(automataGrid grid);
     ruleAutomata(std::initializer_list<uint8_t> birth, std::initializer_list<uint8_t> survival);
+    ruleAutomata(automataRule rule);
     ruleAutomata(std::initializer_list<uint8_t> birth, std::initializer_list<uint8_t> survival, vec2 gridSize, std::deque<std::deque<uint8_t>> grid);
+    ruleAutomata(std::initializer_list<uint8_t> birth, std::initializer_list<uint8_t> survival, automataGrid grid);
+    ruleAutomata(automataRule rule, vec2 gridSize, std::deque<std::deque<uint8_t>> grid);
+    ruleAutomata(automataRule rule, automataGrid grid);
 
     void setRules(std::initializer_list<uint8_t> birth, std::initializer_list<uint8_t> survival);
     void setGrid(std::deque<std::deque<uint8_t>> grid);
     void setCell(int x, int y, uint8_t alive);
     
     void fillGridRandomWithChance(int chance, PractRand::RNGs::Polymorphic::sfc64& rng);
+
+    void simStep();
+    void simSteps(int steps);
+
+    automataGrid &getGrid();
+    automataRule &getRule();
     
 private:
-    std::deque<uint8_t> birth = {3};
-    std::deque<uint8_t> survival = {2, 3};
-    std::deque<std::deque<uint8_t>> grid;
-    vec2 size = {10, 10};
+    std::deque<uint8_t> _birth = {3};
+    std::deque<uint8_t> _survival = {2, 3};
+    std::deque<std::deque<uint8_t>> _grid;
+    vec2 _size = {10, 10};
 };
--- src/cellularAutomata.cpp
+++ src/cellularAutomata.cpp
@@ -1,7 +1,3 @@
-// cellular automata, take in ruleset and a grid of cells, or generate a random grid
-
-#pragma once
-
 #include <deque>
 #include <initializer_list>
 #include "PractRand.h"
@@ -11,50 +7,50 @@
 
 ruleAutomata::ruleAutomata() {
     // Initialize to B3/S23 or the normal life rules, 10x10 grid
-    birth = {3};
-    survival = {2, 3};
-    size = {10, 10};
-    grid.resize(size.y);
-    for (int y = 0; y < size.y; y++) {
-        grid[y].resize(size.x);
-        for (int x = 0; x < size.x; x++) {
-            grid[y][x] = false;
+    _birth = {3};
+    _survival = {2, 3};
+    _size = {10, 10};
+    _grid.resize(_size.y);
+    for (int y = 0; y < _size.y; y++) {
+        _grid[y].resize(_size.x);
+        for (int x = 0; x < _size.x; x++) {
+            _grid[y][x] = false;
         }
     }
 }
-ruleAutomata::ruleAutomata(vec2 gridSize, std::deque<std::deque<uint8_t>> grid): size(gridSize), grid(grid) {
+ruleAutomata::ruleAutomata(vec2 gridSize, std::deque<std::deque<uint8_t>> grid): _size(gridSize), _grid(grid) {
     // Initialize to B3/S23 or the normal life rules, custom grid
-    birth = {3};
-    survival = {2, 3};
+    _birth = {3};
+    _survival = {2, 3};
 }
-ruleAutomata::ruleAutomata(std::initializer_list<uint8_t> birth, std::initializer_list<uint8_t> survival): birth(birth), survival(survival) {
+ruleAutomata::ruleAutomata(std::initializer_list<uint8_t> birth, std::initializer_list<uint8_t> survival): _birth(birth), _survival(survival) {
     // Initialize to custom rules, 10x10 blank grid
-    size = {10, 10};
-    grid.resize(size.y);
-    for (int y = 0; y < size.y; y++) {
-        grid[y].resize(size.x);
-        for (int x = 0; x < size.x; x++) {
-            grid[y][x] = false;
+    _size = {10, 10};
+    _grid.resize(_size.y);
+    for (int y = 0; y < _size.y; y++) {
+        _grid[y].resize(_size.x);
+        for (int x = 0; x < _size.x; x++) {
+            _grid[y][x] = false;
         }
     }
 }
-ruleAutomata::ruleAutomata(std::initializer_list<uint8_t> birth, std::initializer_list<uint8_t> survival, vec2 gridSize, std::deque<std::deque<uint8_t>> grid): birth(birth), survival(survival), size(gridSize), grid(grid) {
+ruleAutomata::ruleAutomata(std::initializer_list<uint8_t> birth, std::initializer_list<uint8_t> survival, vec2 gridSize, std::deque<std::deque<uint8_t>> grid): _birth(birth), _survival(survival), _size(gridSize), _grid(grid) {
     // Initialize to custom rules, custom grid
 }
 
 void ruleAutomata::setRules(std::initializer_list<uint8_t> birth, std::initializer_list<uint8_t> survival) {
-    this->birth = birth;
-    this->survival = survival;
+    this->_birth = birth;
+    this->_survival = survival;
 }
 void ruleAutomata::setGrid(std::deque<std::deque<uint8_t>> grid) {
-    this->grid = grid;
+    this->_grid = grid;
 }
 void ruleAutomata::setCell(int x, int y, uint8_t alive) {
-    grid[y][x] = alive;
+    _grid[y][x] = alive;
 }
     
 void ruleAutomata::fillGridRandomWithChance(int chance, PractRand::RNGs::Polymorphic::sfc64& rng) {
-    for (int x = 0; x < size.x; x++)
-        for (int y = 0; y < size.y; y++)
-            grid[x][y] = rng.raw64() % 100 < chance;
+    for (int x = 0; x < _size.x; x++)
+        for (int y = 0; y < _size.y; y++)
+            _grid[x][y] = rng.raw64() % 100 < chance;
 }
